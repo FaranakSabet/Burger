9e7771d29fe128b3bdf891882a61d02451e38c0b
faranak Sabet <faranaksabet@yahoo.com>
2020-09-15 10:30:42 -0700

    added Schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -9,7 +9,14 @@ CREATE TABLE burgers
     INT,
     burger_name VARCHAR
     (35) NOT NULL,
-    devoured BOOLEAN NOT NULL,
+    devoured BOOLEAN NOT NULL false,
     PRIMARY KEY
     (id)
 );
+
+-- Run the schema.sql and seeds.sql files into the mysql server from the command line.
+-- Make sure you're in the db folder of your app.
+    -- 1. Start MySQL command line tool and login: <mysql -u root -p>.
+    -- 2. With the mysql> command line tool running, enter the command <source schema.sql>. This will run your schema file and all of the queries in it -- in other words, you'll be creating your database.
+    -- 3. Now insert the entries you defined in seeds.sql by running the file: <source seeds.sql>.
+    -- 4. Close out of the MySQL command line tool: <exit>.
