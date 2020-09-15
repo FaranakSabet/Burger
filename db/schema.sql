798a9624b973412f61cef781e9651e8cdbf97d83
faranak Sabet <faranaksabet@yahoo.com>
2020-09-14 23:21:52 -0700

    Updated Server.js

--- db/schema.sql
+++ db/schema.sql
@@ -1,13 +1,15 @@
 DROP DATABASE IF EXISTS burgers_db;
 CREATE burgers_db;
+
 use burgers_db;
 
 CREATE TABLE burgers
 (
     id AUTO_INCREMENT
     INT,
-    burger_name VARCHAR,
-    devoured BOOLEAN,
+    burger_name VARCHAR
+    (35) NOT NULL,
+    devoured BOOLEAN NOT NULL,
     PRIMARY KEY
     (id)
-)
+);
