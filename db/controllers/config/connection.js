c946145d46e4d35c2cf73941879bd9860dcae20b
faranak Sabet <faranaksabet@yahoo.com>
2020-09-15 13:20:06 -0700

    added Connection.js

---
+++ db/controllers/config/connection.js
@@ -0,0 +1,8 @@
+var mysql = require("mysql");
+var connection = mysql.createConnection({
+    port: 3306;
+    host: "localhost",
+    user: "root",
+    password: "Myyear2020",
+    database: "burgers_db"
+});
