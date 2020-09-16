220b37243f553fc88d9422f1ff9cd2ca56a24f0d
faranak Sabet <faranaksabet@yahoo.com>
2020-09-15 17:39:36 -0700

    added Updated server.js

--- server.js
+++ server.js
@@ -1,2 +1,25 @@
 var express = require("express");
 var mysql = require("mysql");
+var bodyParser = require("body-parser");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("viw engine", "handlebars");
+
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+app.listen(PORT, function () {
+  console.log("SERVER IS LISTENING ON PORT 3000");
+});
