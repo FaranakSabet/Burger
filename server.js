155ca64121e2e55dd101a5660b099140895c53ba
faranak Sabet <faranaksabet@yahoo.com>
2020-09-15 17:53:27 -0700

    organized folders and files

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
-var express = require("express");
-var mysql = require("mysql");
-var bodyParser = require("body-parser");
-
-var app = express();
-var PORT = process.env.PORT || 3000;
+var express = require("express"),
+  bodyParser = require("body-parser"),
+  app = express(),
+  PORT = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 
@@ -14,7 +12,7 @@ app.use(bodyParser.json());
 var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("viw engine", "handlebars");
+app.set("view engine", "handlebars");
 
 var routes = require("./controllers/burgers_controller.js");
 
