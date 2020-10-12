58243f57849b5be359cde5111c311042de66d1d8
faranak Sabet <faranaksabet@yahoo.com>
2020-10-11 17:36:05 -0700

    updating burger schema

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -1,51 +1,34 @@
-var express = require("express");
-var burger = require("../model/burger.js");
-var router = express.router();
+let express = require('express')
+let router = express.Router()
 
-router.get("/", function (req, res) {
+let burger = require('../models/burger.js')
+
+router.get('/', (req, res) => {
   burger.selectAll(function (data) {
-    var hbsObject = {
+    let hbsObject = {
       burgers: data,
-    };
-    console.log(hbsObject);
-    res.render("index", hbsObject);
-  });
-});
-router.post("/api/burgers", function (req, res) {
-  burger.create(
-    ["burger_name", "devoured"],
-    [req.body.burger_name, req.body.devoured],
-    function (result) {
-      res.json({ id: result.insertId });
     }
-  );
-});
+    res.render('index', hbsObject)
+  })
+})
 
-router.put("/api/burgers/:id", function (req, res) {
-  var condition = "id = " + req.params.id;
+router.post('/', (req, res) => {
+  burger.insertOne(['burger_name'], [req.body.burger_name], (data) => {
+    res.redirect('/')
+  })
+})
 
-  console.log("condition", condition);
-
-  burger.update({ devoured: req.body.devoured }, condition, function (result) {
-    if (result.changedRows === 0) {
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
+router.post('/:id', (req, res) => {
+  let condition = 'id = ' + req.params.id
+  burger.updateOne(
+    {
+      devoured: true,
+    },
+    condition,
+    (data) => {
+      res.redirect('/')
     }
-  });
-});
-//Deleatig burgers from db
+  )
+})
 
-router.delete("/api/burgers/:id", function (req, res) {
-  var condition = "id = " + req.params.id;
-  console.log("condition", condition);
-
-  burger.delete(condition, function (result) {
-    if (result.changedRows === 0) {
-      return res.result(404).end();
-    } else {
-      res.status(200).end();
-    }
-  });
-});
-module.exports = router;
+module.exports = router
